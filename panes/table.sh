991a8ffd34e13ffde4e984fb1577e8d384bcc406
Cj-bc <cj-bc@y-modify.org>
2018-05-14 00:21:22 +0900

    Change: panes/table> remove non-needed text

--- panes/table.sh
+++ panes/table.sh
@@ -1,6 +1,5 @@
 #!/usr/local/bin/bash
 source funcs.sh
-mkfifo p
 echo "今日の目次" |tee p | ../54ysh/54ysh.sh & box 10 15 1 center < p
 
 echo "1. 自己紹介" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID - 10)) 14 < p
@@ -8,10 +7,6 @@ wait
 echo "2. 主なプロジェクト" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID - 10)) 15 < p
 wait
 echo "3. 各プロジェクトについて" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID - 10)) 16 < p
-wait
-rm p
-
-echo "今日は上記の内容をやる予定でしたが、時間の都合上3.番は無理そう...なので短くなりますがお付き合いください！" | tee vstult.anno.p | ../54ysh/54ysh.sh
 wait
 
 tput cup 1 1
