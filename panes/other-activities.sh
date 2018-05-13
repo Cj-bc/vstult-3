9914d3bff7aa4f4f1a4c3cd3bc3a4b6305a8ebc6
Cj-bc <cj-bc@y-modify.org>
2018-05-14 08:09:29 +0900

    Change: panes/other-activities> ;don't make & remove named-pipe 'p'

--- panes/other-activities.sh
+++ panes/other-activities.sh
@@ -2,10 +2,8 @@
 
 source funcs.sh
 
-mkfifo p
 echo "その他の活動" | tee p | ../54ysh/54ysh.sh & box $(($TERM_Y_MID -5)) 15 1 center < p; wait
 echo "* homebrew formulaの作成" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID -10)) $TERM_Y_MID < p; wait
 echo "* bash補完関数の作成" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID -10)) $(($TERM_Y_MID +2)) < p; wait
 echo "* ドキュメントの翻訳" | tee p | ../54ysh/54ysh.sh & text $(($TERM_X_MID -10)) $(($TERM_Y_MID +4)) < p; wait
-rm p
 tput cup 1 1 
