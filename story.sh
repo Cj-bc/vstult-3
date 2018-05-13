a1154946f46ba70b0e2c4c9aeb10f8273255b241
Cj-bc <cj-bc@y-modify.org>
2018-05-13 16:11:08 +0900

    Add: pane> last pane

--- story.sh
+++ story.sh
@@ -3,7 +3,7 @@
 mesg n # disallow display message
 mkfifo vstult.anno.p
 mkfifo p
-Pages=( "title" "table" "introduction" "main-projects" "other-activities")
+Pages=( "title" "table" "introduction" "main-projects" "other-activities" "last")
 for paneNum in ${Pages[@]}
 do
  while read line # wait until return key was pushed
